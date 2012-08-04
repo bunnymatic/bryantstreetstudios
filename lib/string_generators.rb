798d187a3200452566e5dfa6de301bf9a0e4349a
Mr Rogers <jon@bunnymatic.com>
2012-08-04 15:01:52 -0700

    * fixed up specs

--- lib/string_generators.rb
+++ lib/string_generators.rb
@@ -1,8 +1,7 @@
-letters_plus_space ||= []
-('a'..'z').each {|ltr| letters_plus_space << ltr}
-('A'..'Z').each {|ltr| letters_plus_space << ltr}
-
 def gen_random_string(len=8)
+  letters_plus_space = []
+  ('a'..'z').each {|ltr| letters_plus_space << ltr}
+  ('A'..'Z').each {|ltr| letters_plus_space << ltr}
   numchars = letters_plus_space.length
   (0..len).map{ letters_plus_space[rand(numchars)] }.join
 end
