13fe081a38d70a990fba06b3d0379e226d73d8e2
Mr Rogers <jon@bunnymatic.com>
2015-11-20 19:18:51 -0800

    forgot the mau model

---
+++ models/mau_model.rb
@@ -0,0 +1,28 @@
+require 'byebug'
+class MauModel
+
+  def get_json(path)
+    self.class.get_json(path)
+  end
+
+  def self.get_json(path)
+    MAU::RestClient.get_json( api_url( path ) )
+  end
+
+  def self.base_url
+    config.mau_api_url
+  end
+
+  def self.api_url(path)
+    "#{base_url}/#{path}"
+  end
+
+  def config
+    self.class.config
+  end
+
+  def self.config
+    @config ||= BryantStreetStudios.settings
+  end
+
+end
