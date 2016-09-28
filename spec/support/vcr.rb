db01f558640835de39a4c250a63895d45d2c1c2d
Mr Rogers <jon@bunnymatic.com>
2016-09-28 08:56:04 -0700

    Make vcr use cassettes

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,5 +5,5 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr-cassettes'
   config.configure_rspec_metadata!
   # config.allow_http_connections_when_no_cassette = true
-  config.default_cassette_options = { record: :once }
+  #  config.default_cassette_options = { record: :once }
 end
